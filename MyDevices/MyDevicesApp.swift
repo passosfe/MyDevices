15c6d5cb3c02fd43c5e285fb6c0d15108b8c333a
Felipe Passos - Vortigo <felipe.passos@vortigo.com>
2022-04-22 06:28:47 -0300

    Initial Commit

---
+++ MyDevices/MyDevicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDevicesApp.swift
+//  MyDevices
+//
+//  Created by Felipe Passos on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct MyDevicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
